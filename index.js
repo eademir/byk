95d005fd6f9e23e0f3182007321fe3d8467fa435
eademir <eraydemir1@yandex.com>
2018-08-14 00:56:39 +0300

    .

--- index.js
+++ index.js
@@ -57,4 +57,69 @@ $(document).ready(function(){
     $("#chain").click(function(){
         $("#chaining").css({"backgroundColor": "red", "width": "100px", "height": "100px"}).slideUp(2000).slideDown(2000);/*ya da "width", "100px"*/
     });
+
+    /*javascript if else döngü*/
+    /*var number = 7;
+    var otherNumber = prompt("Aklımdan 1'den 10'a kadar hangi sayıyı tutuyorum, bil bakalım.", "");
+    if (number == otherNumber) {
+        alert("Tebrikler doğru tahmin! =)");
+    } else {
+        alert("Üzgünüm. Tuttuğum sayı 7'ydi.");
+    }*/
+
+    /*$ if else döngü*/
+
+
+    /* slider*/
+    $(function(){
+        var sure = 2000; // slider otomatik dönme süresi
+        var toplamLi = $(".slider ul li").length;
+        var liWidth = 500;
+        var toplamWidth = liWidth * toplamLi;
+        var liDeger = 0;
+        $(".slider ul").css("width", toplamWidth + "px");
+    
+        $("a.sonraki").click(function(){
+            if (liDeger < toplamLi - 1){
+                liDeger++;
+                yeniWidth = liWidth * liDeger;
+                $(".slider ul").animate({marginLeft: "-" + yeniWidth + "px"}, 500);
+            } else {
+                liDeger = 0;
+                $(".slider ul").animate({marginLeft: "0"}, 500);
+            }
+            return false;
+        })
+        $("a.onceki").click(function(){
+            if (liDeger > 0){
+                liDeger--;
+                yeniWidth = liWidth * liDeger;
+                $(".slider ul").animate({marginLeft: "-" + yeniWidth + "px"}, 500);
+            }
+            return false;
+        })
+        /* Otomatik Dönme */
+        $.Slider = function(){
+            if (liDeger < toplamLi - 1){
+                liDeger++;
+                yeniWidth = liWidth * liDeger;
+                $(".slider ul").animate({marginLeft: "-" + yeniWidth + "px"}, 500);
+            } else {
+                liDeger = 0;
+                $(".slider ul").animate({marginLeft: "0"}, 500);
+            }
+        }
+        var don = setInterval("$.Slider()", sure);
+    
+        $("#slider").hover(function(){
+            clearInterval(don);
+        }, function(){
+            don = setInterval("$.Slider()", sure);
+        })
+    });
+
+    //addClass method
+    $("#changeclass").click(function(){
+        $("#changeclass").addClass("changed");
+    })
 });
